67d68389b3fb545776c7af823dd42983f35dff3b
andersonlimadev <andersonlimahw@gmail.com>
2024-07-20 19:07:45 -0300

    🧪 tests: TDD: calculadora

---
+++ models/calculadora.js
@@ -0,0 +1,22 @@
+function somar(a, b) {
+  return a + b;
+}
+
+function substrair(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+exports.calculadora = {
+  somar,
+  substrair,
+  multiplicar,
+  dividir,
+};
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,7 +1,23 @@
-function sum(a, b) {
-  return a + b;
-}
+const { calculadora } = require("../models/calculadora");
 
-test("sum sample", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("calculadora", () => {
+  test("somar 1 + 2 deve retornar 3", () => {
+    expect(calculadora.somar(1, 2)).toBe(3);
+  });
+
+  test("somar 2 + 2 deve retornar 4", () => {
+    expect(calculadora.somar(2, 2)).toBe(4);
+  });
+
+  test("subtrair 2 - 1 deve retornar 1", () => {
+    expect(calculadora.substrair(2, 1)).toBe(1);
+  });
+
+  test("multiplicar 2 * 4 deve retornar 8", () => {
+    expect(calculadora.multiplicar(2, 4)).toBe(8);
+  });
+
+  test("dividir 4 por 2 deve retornar 2", () => {
+    expect(calculadora.dividir(4, 2)).toBe(2);
+  });
 });
