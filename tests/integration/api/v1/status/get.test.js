d86917a6d1fa035113af3cd32b8ddceae00daa54
andersonlimadev <andersonlimahw@gmail.com>
2024-07-20 22:02:47 -0300

    🧪 tests: integration: atualizando integration tests para o endpoint status e code coverage

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 describe("status API", () => {
   test("GET API should return OK (200) status code", async () => {
     const response = await axios("http://localhost:3000/api/v1/status");
-    console.log(response);
     expect(response.statusText).toBe("OK");
   });
 });
