a98b1fb730f8f50f92b0c9fa2f42aef0decd2e30
andersonlimadev <andersonlimahw@gmail.com>
2024-07-24 19:24:11 -0300

    🆕 feature: database: atualizando connection com try, catch e finally

--- infra/database.js
+++ infra/database.js
@@ -10,16 +10,23 @@ async function query() {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
-  const result = await client.query(`
-      SELECT version() as server_version,
-      (SELECT setting from pg_settings WHERE name = 'checkpoint_timeout') AS checkpoint_timeout,
-      (SELECT setting from pg_settings WHERE name = 'max_connections') AS max_connections,
-      (SELECT count(1) from pg_stat_activity) AS opened_connections;
-    `);
-  await client.end();
-  const [row] = result.rows;
-  return row;
+
+  try {
+    await client.connect();
+    const result = await client.query(`
+        SELECT version() as server_version,
+        (SELECT setting from pg_settings WHERE name = 'checkpoint_timeout') AS checkpoint_timeout,
+        (SELECT setting from pg_settings WHERE name = 'max_connections') AS max_connections,
+        (SELECT count(1) from pg_stat_activity) AS opened_connections;
+      `);
+    const [row] = result.rows;
+    return row;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
